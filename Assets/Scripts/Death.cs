02b1816bd5cf3f0df4eb8bc761ee0234f6239d02
Julio Hernández Intriago <juliocesarjedi@gmail.com>
2023-04-29 17:33:21 -0500

    Diseño del nivel 2
    - se propone el diseño del nivel 2
    - se añade un nuevo obstáculo de piso que se cae

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -6,10 +6,13 @@ public class Death : MonoBehaviour {
 
     public int requiredCount = 3; // número de veces que se debe pasar por el trigger
     public int currentCount = 0; // contador de veces que el personaje ha pasado por el trigger
-
-    void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Player")) {
+   // public Transform checkPoint;
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.CompareTag("Player")) {
             currentCount++;
+            //con esta linea cargo desde el punto de chequeo
+            //col.gameObject.GetComponent<CharacterControls>().checkPoint = checkPoint.position;
+            //col.gameObject.GetComponent<CharacterControls>().LoadCheckPoint();
             if (currentCount >= requiredCount) {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
